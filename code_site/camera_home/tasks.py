cc87b7bdb6c463d7c4ca2d5aa0ac56db6ed1260a
Amtrend <alexandermaltsev@bk.ru>
2023-01-22 16:48:47 +0300

    add test ports in csapp

--- code_site/camera_home/tasks.py
+++ code_site/camera_home/tasks.py
@@ -3,11 +3,23 @@ import time
 from celery.contrib.abortable import AbortableTask
 
 
-@shared_task(bind=True, base=AbortableTask)
+# @shared_task(bind=True, base=AbortableTask)
+# def go_test_task(self, test_msg):
+#     try:
+#         while not self.is_aborted():
+#             print(f'i am test task - {test_msg}')
+#             time.sleep(3)
+#     except Exception as e:
+#         print(f'some error : {e}')
+
+
+@shared_task(bind=True)
 def go_test_task(self, test_msg):
     try:
-        while not self.is_aborted():
-            print(f'i am test task - {test_msg}')
-            time.sleep(3)
+        # while not self.is_aborted():
+        print(f'i am test task - {test_msg}')
+        time.sleep(20)
+        print('end')
+        print(self)
     except Exception as e:
         print(f'some error : {e}')
