18aaadcbc83559777fe021b67e073df45150e737
Amtrend <alexandermaltsev@bk.ru>
2023-02-01 13:47:02 +0300

    fix namespaces from celery

--- code_site/smart_home/celery.py
+++ code_site/smart_home/celery.py
@@ -5,4 +5,9 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_home.settings')
 celery_app = Celery('smart_home')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.conf.database_engine_options = {'pool_pre_ping': True}
+celery_app.conf.database_short_lived_sessions = True
+celery_app.conf.broker_transport_options = {'visibility_timeout': 86400}
+celery_app.conf.worker_prefetch_multiplier = 1
+celery_app.conf.worker_max_tasks_per_child = 1
 celery_app.autodiscover_tasks()
