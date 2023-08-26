17a984befa076f58b44fd61de8fde21f3840bae2
Amtrend <alexandermaltsev@bk.ru>
2023-08-26 17:20:27 +0300

    add save data in django (admin)

--- code_site/camera_home/admin.py
+++ code_site/camera_home/admin.py
@@ -19,3 +19,14 @@ class AlarmEntranceSettingsAdmin(admin.ModelAdmin):
     search_fields = ('ae_token',)
     fields = ('ae_on', 'ae_token', 'on_at', 'off_at')
     list_per_page = 10
+
+
+@admin.register(CameraBEntranceSaveVideos)
+class CameraBEntranceSaveVideosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'video', 'start_recording', 'created_at')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    readonly_fields = ('created_at',)
+    fields = ('title', 'video', 'start_recording', 'created_at')
+    list_per_page = 10
+
