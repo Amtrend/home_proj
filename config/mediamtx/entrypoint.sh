beee23b7e798dec0290f4aa05dd481452dcfedaf
amtrend <alexandermaltsev@bk.ru>
2025-07-16 23:10:22 +0300

    fix:bug

--- config/mediamtx/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-envsubst < /mediamtx.template.yml > /mediamtx.yml
-
-echo "========== GENERATED CONFIG =========="
-cat /mediamtx.yml
-echo "======================================"
-
-exec /mediamtx
