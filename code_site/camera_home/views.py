cf455879218e113e6eb6d7ac4f0f8954d57774b6
amtrend <alexandermaltsev@bk.ru>
2025-07-17 20:42:31 +0300

    fix:bug

--- code_site/camera_home/views.py
+++ code_site/camera_home/views.py
@@ -200,7 +200,7 @@ def show_archive_video(request, cam, pk):
     if not file_path.exists():
         raise Http404()
 
-    relative_path = file_path.relative_to(settings.MEDIA_ROOT)
+    relative_path = file_path.relative_to(MEDIA_ROOT)
     response = HttpResponse()
     response['Content-Type'] = 'video/mp4'
     response['X-Accel-Redirect'] = f'/protected_media/{relative_path}'
