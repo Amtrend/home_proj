ea35a11e6a4aefffd351e9ab6a90c6539f416cec
amtrend <alexandermaltsev@bk.ru>
2025-07-18 18:48:17 +0300

    remove env vars

--- code_site/smart_home/celery.py
+++ code_site/smart_home/celery.py
@@ -1,11 +1,13 @@
 import os
+
 from celery import Celery
+from smart_home.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_home.settings')
 celery_app = Celery('smart_home')
-celery_app.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
-celery_app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
+celery_app.conf.broker_url = CELERY_BROKER_URL
+celery_app.conf.result_backend = CELERY_RESULT_BACKEND
 celery_app.conf.worker_prefetch_multiplier = 1
 celery_app.conf.worker_max_tasks_per_child = 1
 celery_app.autodiscover_tasks()
