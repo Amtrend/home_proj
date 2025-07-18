7b7f4b8bea9818e55d50a73b3f80fae0a1b36b8e
amtrend <alexandermaltsev@bk.ru>
2025-07-18 19:29:58 +0300

    fix: bug

--- code_site/smart_home/celery.py
+++ code_site/smart_home/celery.py
@@ -1,13 +1,11 @@
 import os
 
 from celery import Celery
-from smart_home.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_home.settings')
 celery_app = Celery('smart_home')
-celery_app.conf.broker_url = CELERY_BROKER_URL
-celery_app.conf.result_backend = CELERY_RESULT_BACKEND
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.conf.worker_prefetch_multiplier = 1
 celery_app.conf.worker_max_tasks_per_child = 1
 celery_app.autodiscover_tasks()
