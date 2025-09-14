812938c825003babfd10520316905b01a12f6cbd
amtrend <alexandermaltsev@bk.ru>
2025-09-14 12:52:25 +0300

    fix: del videos from cam b entr

--- code_site/camera_home/signals.py
+++ code_site/camera_home/signals.py
@@ -1,10 +1,16 @@
 from django.db.models.signals import pre_delete
-from .models import CameraEntranceSaveVideos
+from .models import CameraEntranceSaveVideos, CameraBEntranceSaveVideos
 from django.dispatch.dispatcher import receiver
 
 
 @receiver(pre_delete, sender=CameraEntranceSaveVideos)
-def video_post_delete_handler(sender, **kwargs):
+def video_post_delete_cam_handler(sender, **kwargs):
+    video = kwargs["instance"]
+    storage, path = video.video.storage, video.video.path
+    storage.delete(path)
+
+@receiver(pre_delete, sender=CameraBEntranceSaveVideos)
+def video_post_delete_cam_b_handler(sender, **kwargs):
     video = kwargs["instance"]
     storage, path = video.video.storage, video.video.path
     storage.delete(path)
