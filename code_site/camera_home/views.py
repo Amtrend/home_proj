6220269504b620ad336c4e92780cd8aa9cd233a1
amtrend <alexandermaltsev@bk.ru>
2025-09-18 11:56:49 +0300

    feature: add filters

--- code_site/camera_home/views.py
+++ code_site/camera_home/views.py
@@ -121,7 +121,7 @@ def cam_archive_page(request, cam):
     if from_date:
         queryset = queryset.filter(start_recording__gte=from_date)
     if to_date:
-        queryset = queryset.filter(created_at__lte=to_date)
+        queryset = queryset.filter(start_recording__lte=to_date)
 
     ce_videos = queryset.order_by('-created_at')
 
